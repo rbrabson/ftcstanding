ab900a077727c8d1e84e7f6b8d659210c1cad494
Roy Brabson <brabson@alumni.duke.edu>
2026-02-06 13:37:35 -0500

    initial

--- cli/teams.go
+++ cli/teams.go
@@ -1,48 +1,37 @@
 package cli
 
 import (
-	"bytes"
-
-	"github.com/fatih/color"
+	"github.com/jedib0t/go-pretty/table"
 	"github.com/rbrabson/ftcstanding/database"
 	"github.com/rbrabson/ftcstanding/query"
-	"github.com/rodaine/table"
 )
 
-// RenderTeams renders a list of teams as a table.
 func RenderTeams(teams []*database.Team) string {
-	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-	columnFmt := color.New(color.FgYellow).SprintfFunc()
-
-	tbl := table.New("ID", "Name", "Country", "Region", "Rookie Year")
-	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	tw := table.NewWriter()
+	tw.AppendHeader(table.Row{"ID", "Name", "Country", "Region", "Rookie Year"})
 
 	for _, team := range teams {
-		tbl.AddRow(team.TeamID, team.Name, team.Country, team.HomeRegion, team.RookieYear)
+		tw.AppendRow(
+			table.Row{team.TeamID, team.Name, team.Country, team.HomeRegion, team.RookieYear},
+		)
 	}
 
-	buffer := &bytes.Buffer{}
-	tbl.WithWriter(buffer).Print()
-	return buffer.String()
+	return tw.Render()
 }
 
 // RenderTeamMatchDetails renders a list of team match details as a table.
 func RenderTeamMatchDetails(details []*query.TeamMatchDetails) string {
-	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-	columnFmt := color.New(color.FgYellow).SprintfFunc()
 
-	tbl := table.New("Event", "Match", "Alliance", "Total Points", "Teams")
-	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
+	tw := table.NewWriter()
+	tw.AppendHeader(table.Row{"Event", "Match", "Alliance", "Total Points", "Teams"})
 
 	for _, detail := range details {
 		teamIDs := make([]int, 0, len(detail.Teams))
 		for _, mt := range detail.Teams {
 			teamIDs = append(teamIDs, mt.TeamID)
 		}
-		tbl.AddRow(detail.Event.EventCode, detail.Match.MatchNumber, detail.AllianceScore.Alliance, detail.AllianceScore.TotalPoints, teamIDs)
+		tw.AppendRow(table.Row{detail.Event.EventCode, detail.Match.MatchNumber, detail.AllianceScore.Alliance, detail.AllianceScore.TotalPoints, teamIDs})
 	}
 
-	buffer := &bytes.Buffer{}
-	tbl.WithWriter(buffer).Print()
-	return buffer.String()
+	return tw.Render()
 }
--- go.mod
+++ go.mod
@@ -3,16 +3,25 @@ module github.com/rbrabson/ftcstanding
 go 1.24.0
 
 require (
-	github.com/fatih/color v1.18.0
 	github.com/go-sql-driver/mysql v1.9.3
+	github.com/jedib0t/go-pretty v4.3.0+incompatible
 	github.com/joho/godotenv v1.5.1
 	github.com/rbrabson/ftc v0.0.0-20260205181238-6917ef8ce55b
-	github.com/rodaine/table v1.3.0
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.5.0 // indirect
+	github.com/go-openapi/errors v0.22.6 // indirect
+	github.com/go-openapi/strfmt v0.25.0 // indirect
+	github.com/go-viper/mapstructure/v2 v2.5.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	go.mongodb.org/mongo-driver v1.17.8 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
 )
