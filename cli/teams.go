89b174e81434fe671a65a6f947d9cda6c696ea22
Roy Brabson <brabson@alumni.duke.edu>
2026-02-06 11:55:02 -0500

    initial

--- cli/teams.go
+++ cli/teams.go
@@ -13,11 +13,11 @@ func RenderTeams(teams []*database.Team) string {
 	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
 	columnFmt := color.New(color.FgYellow).SprintfFunc()
 
-	tbl := table.New("ID", "Name", "Country", "Region")
+	tbl := table.New("ID", "Name", "Country", "Region", "Rookie Year")
 	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
 
 	for _, team := range teams {
-		tbl.AddRow(team.TeamID, team.Name, team.Country, team.HomeRegion)
+		tbl.AddRow(team.TeamID, team.Name, team.Country, team.HomeRegion, team.RookieYear)
 	}
 
 	buffer := &bytes.Buffer{}
