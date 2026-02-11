ab4d9cf34c00f2fd4b1146196f015c53fe7b1c0c
Roy Brabson <brabson@alumni.duke.edu>
2026-02-11 12:41:46 -0500

    initial

--- request/lambda.go
+++ request/lambda.go
@@ -6,16 +6,11 @@ import (
 
 // getLambda computes the recommended lambda value for regularization based on the match data.
 func getLambda(matches []performance.Match) float64 {
-	lambda := baseLambda(len(matches))
-	return lambda
-}
-
-// BaseLambda computes the base lambda value based on the number of matches.
-func baseLambda(matchCount int) float64 {
+	matcheCount := len(matches)
 	switch {
-	case matchCount < 20:
+	case matcheCount < 20:
 		return 0.1
-	case matchCount <= 60:
+	case matcheCount <= 60:
 		return 0.01
 	default:
 		return 0.001
